b141246ac43604e008ece55e59714c66b42d29e4
Fándly Gergő <gergo@paymoapp.com>
2022-10-28 09:27:37 +0300

    imp(module.macos): Simplified getSystemIdleSeconds function based on Chromium source code

--- module/macos/RealIdle.mm
+++ module/macos/RealIdle.mm
@@ -23,29 +23,9 @@ namespace PaymoRealIdle {
 	}
 
 	int32_t getSystemIdleSeconds() {
-		int32_t idleSeconds = -1;
-		io_iterator_t iter = 0;
+		CFTimeInterval idleTime = CGEventSourceSecondsSinceLastEventType(kCGEventSourceStateCombinedSessionState, kCGAnyInputEventType);
 
-		if (IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching("IOHIDSystem"), &iter) == KERN_SUCCESS) {
-			io_registry_entry_t entry = IOIteratorNext(iter);
-			if (entry) {
-				CFMutableDictionaryRef dict = NULL;
-				if (IORegistryEntryCreateCFProperties(entry, &dict, kCFAllocatorDefault, 0) == KERN_SUCCESS) {
-					CFNumberRef obj = reinterpret_cast<CFNumberRef>(CFDictionaryGetValue(dict, CFSTR("HIDIdleTime")));
-					if (obj) {
-						int64_t nanoseconds = 0;
-						if (CFNumberGetValue(obj, kCFNumberSInt64Type, &nanoseconds)) {
-							idleSeconds = static_cast<int32_t>(nanoseconds / NSEC_PER_SEC);
-						}
-					}
-					CFRelease(dict);
-				}
-				IOObjectRelease(entry);
-			}
-			IOObjectRelease(iter);
-		}
-
-		return idleSeconds;
+		return static_cast<int32_t>(idleTime);
 	}
 
 	bool getSystemIdlePrevented() {
